63a02adf2f8a6a684d2ca13c54306e2aafb7aefd
Tony McGee <tmcgee.au@gmail.com>
2022-03-06 21:46:07 +1000

    Remove/rotate secret key

--- bucket_peek/bucket_peek/settings.py
+++ bucket_peek/bucket_peek/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ulg*)bpp+y_hh=w^1#%t236w1_%7$mo*-74dwrkjmbe7ai8=yf'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
